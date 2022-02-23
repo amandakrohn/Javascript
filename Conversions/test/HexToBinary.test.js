58c70fba5ee3406d37f9a3fedff76ec7cb8bff5b
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2022-02-23 13:59:06 +0100

    feat: add tests that cover all branches

--- Conversions/test/HexToBinary.test.js
+++ Conversions/test/HexToBinary.test.js
@@ -16,4 +16,14 @@ describe('hexToBinary', () => {
   it('expects to return correct hexadecimal value, matching (num).toString(2)', () => {
     expect(hexToBinary('F')).toBe(parseInt('F', 16).toString(2))
   })
+
+  // We expect each hexadecimal digit to transform into its binary 4-digit part.
+  it('expects to return correct binary representation for all branches', () => {
+    expect(hexToBinary('0123456789ABCDEFabcdef')).toBe('0000000100100011010001010110011110001001101010111100110111101111101010111100110111101111')
+  })
+
+  // We expect an invalid hexadecimal digit to return empty string
+  it('expect to return empty string for invalid hexadecimal digit', () => {
+    expect(hexToBinary('H')).toBe('')
+  })
 })
