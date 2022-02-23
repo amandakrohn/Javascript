e3e77641b2974a391de4a9c99ec41c5c5ea03399
darkways <51851705+darkway-s@users.noreply.github.com>
2022-02-23 21:12:31 +0100

    change lines don't affect the number of branches in auto measurement

--- Maths/SimpsonIntegration.js
+++ Maths/SimpsonIntegration.js
@@ -24,7 +24,7 @@
 function integralEvaluation (N, a, b, func) {
   // Check if N is an even integer
   let isNEven = true
-  if (N % 2 !== 0) isNEven = false
+  if (N % 2 !== 0) {isNEven = false}
 
   if (!Number.isInteger(N) || Number.isNaN(a) || Number.isNaN(b)) { throw new TypeError('Expected integer N and finite a, b') }
   if (!isNEven) { throw Error('N is not an even number') }
@@ -44,7 +44,8 @@ function integralEvaluation (N, a, b, func) {
   // Find the sum {f(x0) + 4*f(x1) + 2*f(x2) + ... + 2*f(xN-2) + 4*f(xN-1) + f(xN)}
   let temp
   for (let i = 0; i < N + 1; i++) {
-    if (i === 0 || i === N) temp = func(xi)
+    if (i === 0) temp = func(xi)
+    else if(i === N) temp = func(xi)
     else if (i % 2 === 0) temp = 2 * func(xi)
     else temp = 4 * func(xi)
 
