2f9a57d81bedf511041fd05f1974fd235dc629df
Sefik-Palazoglu <sefik.palazoglu.s@gmail.com>
2022-02-23 13:19:27 +0100

    feat: implement coverage test for HexToBinary.js

--- Conversions/test/HexToBinary.test.js
+++ Conversions/test/HexToBinary.test.js
@@ -1,19 +1,27 @@
 import hexToBinary from '../HexToBinary'
 
 describe('hexToBinary', () => {
+  let coverage = null
+  beforeAll(() => {
+    coverage = new Array(17).fill(false)
+  })
   it('expects to return correct hexadecimal value', () => {
-    expect(hexToBinary('8')).toBe('1000')
+    expect(hexToBinary('8', coverage)).toBe('1000')
   })
 
   it('expects to return correct binary value for more than one hex digit', () => {
-    expect(hexToBinary('EA')).toBe('11101010')
+    expect(hexToBinary('EA', coverage)).toBe('11101010')
   })
 
   it('expects to test its robustness as it should be case-insensitive', () => {
-    expect(hexToBinary('4d')).toBe('01001101')
+    expect(hexToBinary('4d', coverage)).toBe('01001101')
   })
 
   it('expects to return correct hexadecimal value, matching (num).toString(2)', () => {
-    expect(hexToBinary('F')).toBe(parseInt('F', 16).toString(2))
+    expect(hexToBinary('F', coverage)).toBe(parseInt('F', 16).toString(2))
+  })
+  afterAll(() => {
+    for (let i = 0; i < coverage.length; i++) { console.log('Branch ' + i + ' taken: ' + coverage[i]) }
+    console.log(coverage)
   })
 })
