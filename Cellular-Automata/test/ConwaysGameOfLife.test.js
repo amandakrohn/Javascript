4a5f0a6533b3fb9a1251f06297fc2fa912c02f68
Amanda Krohn <akrohn@kth.se>
2022-02-24 07:35:25 +0100

    Adds manual branch coverage tool, fix #12

--- Cellular-Automata/test/ConwaysGameOfLife.test.js
+++ Cellular-Automata/test/ConwaysGameOfLife.test.js
@@ -1,8 +1,18 @@
 import { newGeneration } from '../ConwaysGameOfLife'
 
 describe('newGeneration', () => {
+  let coverage = null
+    beforeEach(() => {
+      coverage = new Array(17).fill(false)
+  })
+  afterEach(() => {
+    for (let i = 0; i < coverage.length; i++) {
+      console.log('Branch ' + i + ' taken: ' + coverage[i]) 
+    }
+  })
+  
   it('should produce the next generation according to the rules', () => {
-    expect(newGeneration([[0, 1, 0], [0, 1, 0], [0, 1, 0]]))
+    expect(newGeneration([[0, 1, 0], [0, 1, 0], [0, 1, 0]], coverage))
       .toEqual([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
   })
 })
