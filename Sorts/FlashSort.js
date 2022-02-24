215372345befe91cd2c70f7b481c0520696606d4
Ludwig Johansson <johanssons.ludwig@gmail.com>
2022-02-24 08:02:01 +0100

    Added DIY coverage tool to falshSort, feat #13

--- Sorts/FlashSort.js
+++ Sorts/FlashSort.js
@@ -6,22 +6,26 @@
  * Wikipedia: https://en.wikipedia.org/wiki/Flashsort
  */
 
-export function flashSort (arr) {
+export function flashSort (arr, coverage) {
   let max = 0; let min = arr[0]
   const n = arr.length
   const m = ~~(0.45 * n)
   const l = new Array(m)
 
   for (let i = 1; i < n; ++i) {
+    coverage[0] = true
     if (arr[i] < min) {
       min = arr[i]
+      coverage[1] = true
     }
     if (arr[i] > arr[max]) {
       max = i
+      coverage[2] = true
     }
   }
 
   if (min === arr[max]) {
+    coverage[3] = true
     return arr
   }
 
@@ -29,15 +33,18 @@ export function flashSort (arr) {
 
   for (let k = 0; k < m; k++) {
     l[k] = 0
+    coverage[4] = true
   }
 
   for (let j = 0; j < n; ++j) {
     const k = ~~(c1 * (arr[j] - min))
     ++l[k]
+    coverage[5] = true
   }
 
   for (let p = 1; p < m; ++p) {
     l[p] = l[p] + l[p - 1]
+    coverage[6] = true
   }
 
   let hold = arr[max]
@@ -50,11 +57,16 @@ export function flashSort (arr) {
   let k = m - 1
 
   while (move < (n - 1)) {
+    coverage[7] = true
     while (j > (l[k] - 1)) {
       ++j
       k = ~~(c1 * (arr[j] - min))
+      coverage[8] = true
     }
-    if (k < 0) break
+    if (k < 0) {
+      coverage[9] = true
+      break
+    }
     flash = arr[j]
     while (j !== l[k]) {
       k = ~~(c1 * (flash - min))
@@ -62,15 +74,18 @@ export function flashSort (arr) {
       arr[t] = flash
       flash = hold
       ++move
+      coverage[10] = true
     }
   }
 
   // insertion
   for (j = 1; j < n; j++) {
+    coverage[11] = true
     hold = arr[j]
     let i = j - 1
     while (i >= 0 && arr[i] > hold) {
       arr[i + 1] = arr[i--]
+      coverage[12] = true
     }
     arr[i + 1] = hold
   }
