c79ee12d3ddb991dce1265328391843ac9cd4a85
Amanda Krohn <33667375+amandakrohn@users.noreply.github.com>
2022-02-25 15:36:49 +0100

    Adds DIY branch coverage tool to MaxProductOfThree.js. Fixes #39 (#42)

--- Dynamic-Programming/MaxProductOfThree.js
+++ Dynamic-Programming/MaxProductOfThree.js
@@ -5,10 +5,14 @@
  * @param {number[]} arrayItems
  * @returns number
  */
-export function maxProductOfThree (arrayItems) {
+export function maxProductOfThree (arrayItems, coverage) {
   // if size is less than 3, no triplet exists
   const n = arrayItems.length
-  if (n < 3) throw new Error('Triplet cannot exist with the given array')
+  if (n < 3) {
+    coverage[0] = true
+    throw new Error('Triplet cannot exist with the given array')
+  } else coverage[1] = true
+
   let max1 = arrayItems[0]
   let max2 = -1
   let max3 = -1
@@ -16,22 +20,33 @@ export function maxProductOfThree (arrayItems) {
   let min2 = -1
   for (let i = 1; i < n; i++) {
     if (arrayItems[i] > max1) {
+      coverage[2] = true
       max3 = max2
       max2 = max1
       max1 = arrayItems[i]
     } else if (max2 === -1 || arrayItems[i] > max2) {
+      coverage[3] = true
       max3 = max2
       max2 = arrayItems[i]
     } else if (max3 === -1 || arrayItems[i] > max3) {
+      coverage[4] = true
       max3 = arrayItems[i]
+    } else {
+      coverage[5] = true
     }
+
     if (arrayItems[i] < min1) {
+      coverage[6] = true
       min2 = min1
       min1 = arrayItems[i]
     } else if (min2 === -1 || arrayItems[i] < min2) {
+      coverage[7] = true
       min2 = arrayItems[i]
+    } else {
+      coverage[8] = true
     }
   }
+
   const prod1 = max1 * max2 * max3
   const prod2 = max1 * min1 * min2
   return Math.max(prod1, prod2)
