51d1251c6d5d9b3472220545453f78b6d13d60f3
Amanda Krohn <akrohn@kth.se>
2022-02-25 10:10:10 +0100

    Adds DIY branch coverage tool to MaxProductOfThree.js

--- Dynamic-Programming/MaxProductOfThree.js
+++ Dynamic-Programming/MaxProductOfThree.js
@@ -5,10 +5,14 @@
  * @param {number[]} arrayItems
  * @returns number
  */
-export function maxProductOfThree (arrayItems) {
+export function maxProductOfThree (arrayItems, coverage) {
   // if size is less than 3, no triplet exists
   const n = arrayItems.length
-  if (n < 3) throw new Error('Triplet cannot exist with the given array')
+  if (n < 3) {
+    coverage[0] = true
+    throw new Error('Triplet cannot exist with the given array')
+  }
+
   let max1 = arrayItems[0]
   let max2 = -1
   let max3 = -1
@@ -16,19 +20,24 @@ export function maxProductOfThree (arrayItems) {
   let min2 = -1
   for (let i = 1; i < n; i++) {
     if (arrayItems[i] > max1) {
+      coverage[1] = true
       max3 = max2
       max2 = max1
       max1 = arrayItems[i]
     } else if (max2 === -1 || arrayItems[i] > max2) {
+      coverage[2] = true
       max3 = max2
       max2 = arrayItems[i]
     } else if (max3 === -1 || arrayItems[i] > max3) {
+      coverage[3] = true
       max3 = arrayItems[i]
     }
     if (arrayItems[i] < min1) {
+      coverage[4] = true
       min2 = min1
       min1 = arrayItems[i]
     } else if (min2 === -1 || arrayItems[i] < min2) {
+      coverage[5] = true
       min2 = arrayItems[i]
     }
   }
--- Dynamic-Programming/tests/MaxProductOfThree.test.js
+++ Dynamic-Programming/tests/MaxProductOfThree.test.js
@@ -1,17 +1,31 @@
-import { maxProductOfThree } from '../MaxProductOfThree'
+import {
+  maxProductOfThree
+} from '../MaxProductOfThree'
 
 describe('MaxProductOfThree', () => {
+  let coverage = null
+  beforeAll(() => {
+    coverage = new Array(5).fill(false)
+  })
+
   it('expects to throw error for array with only 2 numbers', () => {
     expect(() => {
-      maxProductOfThree([1, 3])
+      maxProductOfThree([1, 3], coverage)
     }).toThrow('Triplet cannot exist with the given array')
   })
 
   it('expects to return 300 as the maximum product', () => {
-    expect(maxProductOfThree([10, 6, 5, 3, 1, -10])).toBe(300)
+    expect(maxProductOfThree([10, 6, 5, 3, 1, -10], coverage)).toBe(300)
   })
 
   it('expects to return 300 as the maximum product', () => {
-    expect(maxProductOfThree([10, -6, 5, 3, 1, -10])).toBe(600)
+    expect(maxProductOfThree([10, -6, 5, 3, 1, -10], coverage)).toBe(600)
+  })
+
+  afterAll(() => {
+    for (let i = 0; i < coverage.length; i++) {
+      console.log('Branch ' + i + ' taken: ' + coverage[i])
+    }
+    console.log(coverage)
   })
 })
