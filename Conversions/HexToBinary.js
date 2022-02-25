bdc1c9b53f7e0e53b21633e44f351dc3cb43539a
Sefik-Palazoglu <44020012+Sefik-Palazoglu@users.noreply.github.com>
2022-02-25 16:20:05 +0100

    DIY coverage implementation for HexToBinary.js (#46)
    * Auto-update DIRECTORY.md
    
    * feat: implement coverage test for HexToBinary.js
    
    Co-authored-by: github-actions <${GITHUB_ACTOR}@users.noreply.github.com>

--- Conversions/HexToBinary.js
+++ Conversions/HexToBinary.js
@@ -1,25 +1,59 @@
-const binLookup = (c) => {
+const binLookup = (c, coverage) => {
   switch (c.toLowerCase()) {
-    case '0': return '0000'
-    case '1': return '0001'
-    case '2': return '0010'
-    case '3': return '0011'
-    case '4': return '0100'
-    case '5': return '0101'
-    case '6': return '0110'
-    case '7': return '0111'
-    case '8': return '1000'
-    case '9': return '1001'
-    case 'a': return '1010'
-    case 'b': return '1011'
-    case 'c': return '1100'
-    case 'd': return '1101'
-    case 'e': return '1110'
-    case 'f': return '1111'
-    default: return ''
+    case '0':
+      coverage[0] = true
+      return '0000'
+    case '1':
+      coverage[1] = true
+      return '0001'
+    case '2':
+      coverage[2] = true
+      return '0010'
+    case '3':
+      coverage[3] = true
+      return '0011'
+    case '4':
+      coverage[4] = true
+      return '0100'
+    case '5':
+      coverage[5] = true
+      return '0101'
+    case '6':
+      coverage[6] = true
+      return '0110'
+    case '7':
+      coverage[7] = true
+      return '0111'
+    case '8':
+      coverage[8] = true
+      return '1000'
+    case '9':
+      coverage[9] = true
+      return '1001'
+    case 'a':
+      coverage[10] = true
+      return '1010'
+    case 'b':
+      coverage[11] = true
+      return '1011'
+    case 'c':
+      coverage[12] = true
+      return '1100'
+    case 'd':
+      coverage[13] = true
+      return '1101'
+    case 'e':
+      coverage[14] = true
+      return '1110'
+    case 'f':
+      coverage[15] = true
+      return '1111'
+    default:
+      coverage[16] = true
+      return ''
   }
 }
-const hexToBinary = (hexString) => {
+const hexToBinary = (hexString, coverage = new Array(17).fill(false)) => {
   /*
     Function for converting Hex to Binary
 
@@ -28,7 +62,7 @@ const hexToBinary = (hexString) => {
 
     */
   const hexLexemes = hexString.split('')
-  return hexLexemes.map(lexeme => binLookup(lexeme)).join('')
+  return hexLexemes.map(lexeme => binLookup(lexeme, coverage)).join('')
 }
 
 export default hexToBinary
