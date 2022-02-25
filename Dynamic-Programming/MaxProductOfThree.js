55c08fbe160447d93e8cb55b3c720c4399a534aa
Amanda Krohn <akrohn@kth.se>
2022-02-25 15:02:28 +0100

    Adds coverage tool to to-be-added else-statements

--- Dynamic-Programming/MaxProductOfThree.js
+++ Dynamic-Programming/MaxProductOfThree.js
@@ -11,7 +11,7 @@ export function maxProductOfThree (arrayItems, coverage) {
   if (n < 3) {
     coverage[0] = true
     throw new Error('Triplet cannot exist with the given array')
-  }
+  } else coverage[1] = true
 
   let max1 = arrayItems[0]
   let max2 = -1
@@ -20,27 +20,33 @@ export function maxProductOfThree (arrayItems, coverage) {
   let min2 = -1
   for (let i = 1; i < n; i++) {
     if (arrayItems[i] > max1) {
-      coverage[1] = true
+      coverage[2] = true
       max3 = max2
       max2 = max1
       max1 = arrayItems[i]
     } else if (max2 === -1 || arrayItems[i] > max2) {
-      coverage[2] = true
+      coverage[3] = true
       max3 = max2
       max2 = arrayItems[i]
     } else if (max3 === -1 || arrayItems[i] > max3) {
-      coverage[3] = true
-      max3 = arrayItems[i]
-    }
-    if (arrayItems[i] < min1) {
       coverage[4] = true
+      max3 = arrayItems[i]
+    } else {
+      coverage[5] = true
+    }
+
+    if (arrayItems[i] < min1) {
+      coverage[6] = true
       min2 = min1
       min1 = arrayItems[i]
     } else if (min2 === -1 || arrayItems[i] < min2) {
-      coverage[5] = true
+      coverage[7] = true
       min2 = arrayItems[i]
+    } else {
+      coverage[8] = true
     }
   }
+
   const prod1 = max1 * max2 * max3
   const prod2 = max1 * min1 * min2
   return Math.max(prod1, prod2)
