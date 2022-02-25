ea086b66bbf5603feaec47be6f386b0a2d192e82
Ludwig Johansson <johanssons.ludwig@gmail.com>
2022-02-25 15:34:09 +0100

    DIY coverage tool for hexLookup() and flashSort(), fixes #13 (#24)
    * Adds DIY coverage tool to flashSort,
    * Adds DIY coverage test to BinaryToHex.js
    * Tests and verifcation of branch coverage to BinaryToHex
    * Fixes styling for BinaryToHex test

--- Conversions/BinaryToHex.js
+++ Conversions/BinaryToHex.js
@@ -3,31 +3,64 @@ const pad = (num, padlen) => {
   return (pad + num).slice(-pad.length)
 }
 
-const hexLookup = (bin) => {
+const hexLookup = (bin, coverage) => {
   let binary = bin
   if (binary.length < 4) {
     binary = pad(binary, 4)
+    coverage[0] = true
   }
   switch (binary) {
-    case '0000': return '0'
-    case '0001': return '1'
-    case '0010': return '2'
-    case '0011': return '3'
-    case '0100': return '4'
-    case '0101': return '5'
-    case '0110': return '6'
-    case '0111': return '7'
-    case '1000': return '8'
-    case '1001': return '9'
-    case '1010': return 'A'
-    case '1011': return 'B'
-    case '1100': return 'C'
-    case '1101': return 'D'
-    case '1110': return 'E'
-    case '1111': return 'F'
+    case '0000':
+      coverage[1] = true
+      return '0'
+    case '0001':
+      coverage[2] = true
+      return '1'
+    case '0010':
+      coverage[3] = true
+      return '2'
+    case '0011':
+      coverage[4] = true
+      return '3'
+    case '0100':
+      coverage[5] = true
+      return '4'
+    case '0101':
+      coverage[6] = true
+      return '5'
+    case '0110':
+      coverage[7] = true
+      return '6'
+    case '0111':
+      coverage[8] = true
+      return '7'
+    case '1000':
+      coverage[9] = true
+      return '8'
+    case '1001':
+      coverage[10] = true
+      return '9'
+    case '1010':
+      coverage[11] = true
+      return 'A'
+    case '1011':
+      coverage[12] = true
+      return 'B'
+    case '1100':
+      coverage[13] = true
+      return 'C'
+    case '1101':
+      coverage[14] = true
+      return 'D'
+    case '1110':
+      coverage[15] = true
+      return 'E'
+    case '1111':
+      coverage[16] = true
+      return 'F'
   }
 }
-const binaryToHex = (binaryString) => {
+const binaryToHex = (binaryString, coverage) => {
   /*
   Function for convertung Binary to Hex
 
@@ -48,9 +81,9 @@ const binaryToHex = (binaryString) => {
   binaryString = binaryString.split('')
   for (let i = binaryString.length - 1; i >= 0; i = i - 4) {
     if (i >= 3) {
-      result += hexLookup(binaryString.slice(i - 3, i + 1).join(''))
+      result += hexLookup(binaryString.slice(i - 3, i + 1).join(''), coverage)
     } else {
-      result += hexLookup(binaryString.slice(0, i + 1).join(''))
+      result += hexLookup(binaryString.slice(0, i + 1).join(''), coverage)
     }
   }
   return result.split('').reverse().join('')
--- Conversions/test/BinaryToHex.test.js
+++ Conversions/test/BinaryToHex.test.js
@@ -1,19 +1,36 @@
 import binaryToHex from '../BinaryToHex'
 
 describe('BinaryToHex', () => {
+  let coverage = null
+  beforeAll(() => {
+    coverage = new Array(17).fill(false)
+  })
+
   it('expects to return correct hexadecimal value', () => {
-    expect(binaryToHex('1000')).toBe('8')
+    expect(binaryToHex('1000', coverage)).toBe('8')
   })
 
   it('expects to return correct hexadecimal value for more than one hex digit', () => {
-    expect(binaryToHex('11101010')).toBe('EA')
+    expect(binaryToHex('11101010', coverage)).toBe('EA')
   })
 
   it('expects to return correct hexadecimal value for padding-required binary', () => {
-    expect(binaryToHex('1001101')).toBe('4D')
+    expect(binaryToHex('1001101', coverage)).toBe('4D')
   })
 
   it('expects to return correct hexadecimal value, matching (num).toString(16)', () => {
-    expect(binaryToHex('1111')).toBe(parseInt('1111', 2).toString(16).toUpperCase())
+    expect(binaryToHex('1111', coverage)).toBe(parseInt('1111', 2).toString(16).toUpperCase())
+  })
+
+  it('expect to return correct hexadecimal value for all non decimal numbers', () => {
+    expect(binaryToHex('11111110110111001011', coverage)).toBe('FEDCB')
+  })
+
+  it('expects to return correct hexadecimal value for all decimal numbers', () => {
+    expect(binaryToHex('10101001100001110110010101000011001000010000', coverage)).toBe('A9876543210')
+  })
+
+  afterAll(() => {
+    console.log('BinaryToHex coverage:', coverage)
   })
 })
